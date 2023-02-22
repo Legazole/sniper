e7226ac683bfa4efb3bf4056eaf2b0407adc26b3
Wouter Sels <PhaeroCode@hotmail.com>
2023-02-22 19:18:37 +0100

    remade script

---
+++ bSniper/scripts/priceFeed.js
@@ -0,0 +1,13 @@
+const { getNamedAccounts, ethers } = require("hardhat")
+
+async function main() {
+    //we start anew seeing that my ubuntu distro is dead, need to remove the partition
+    //make a new bigger partition for dev purposes
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error)
+        process.exit(1)
+    })
